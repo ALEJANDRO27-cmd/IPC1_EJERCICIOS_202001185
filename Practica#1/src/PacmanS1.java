1f9c6b4eda595bf86793d3a261045de1138462bd
ALEJANDRO27-cmd <alejandro27gj@gmail.com>
2026-02-20 19:13:17 -0600

    practica_1

--- Practica#1/src/PacmanS1.java
+++ Practica#1/src/PacmanS1.java
@@ -2,25 +2,31 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class PacmanS1 {
-    //tamaños del tablero de juego
+    // Tamaños del tablero de juego
     private static final int FILAS_PEQUENO = 5;
     private static final int COLUMNAS_PEQUENO = 6;
     private static final int FILAS_GRANDE = 10;
     private static final int COLUMNAS_GRANDE = 10;
     
-    //variables dependen del tamaño que el usuario elija
+    // Variables dependen del tamaño que el usuario elija
     private static int filasActuales;
     private static int columnasActuales;
     private static String[][] tablero;
     
-    //variables al empezar el juego
-    private static char tamaño = ' '; //esta almacena sola 1 letra
-    private static  String nombreJugador = "";
-    private int puntos = 0;
-    private int vidas = 3;
+    // Variables al empezar el juego
+    private static char tamaño = ' '; 
+    private static String nombreJugador = "";
+    
+    // --- CORRECCIÓN: Se agregó 'static' a estas variables para que no den error ---
+    private static int puntos = 0;
+    private static int vidas = 3;
     private static boolean jugando = true;
     
-    //variables de los personajes
+    // Localización del pacman (Para saber dónde está siempre)
+    private static int pacmanFila;
+    private static int pacmanCol;
+    
+    // Variables de los personajes
     private static final String fantasma = "@";
     private static final String premio = "0";
     private static final String premio_especial = "$";
@@ -34,7 +40,6 @@ public class PacmanS1 {
         System.out.println("BIENVENIDO A PAC-MAN :V");
         System.out.println("-------------------------");
         int opcionInicio;
-        // \n hace salto de linea 
         System.out.println("ELIJA LAS SIGUIENTES OPCIONES");  
         System.out.println("\nElija 1 para crear el tablero (CONFIGURACION DEL  TABLERO)");
         System.out.println("Elija 2 para ver los puntos (HISTORIAL DE PUNTOS)");
@@ -45,122 +50,162 @@ public class PacmanS1 {
 
         System.out.println("\nGracias por jugar PAC-MAN :,V");
     }
-    // este proceso indica que opciones tiene el usuario en este caso son 3
+
     public static void opcionElegida(int opcionInicio){
-        
-            int numeroPremios;
-            int numeroParedes;
-            int numeroTrampas;
+        int numeroPremios;
+        int numeroParedes;
+        int numeroTrampas;
         
         switch (opcionInicio) {
-                case 1:
-                    sc.nextLine(); //evita que de enter automatico
-                    System.out.println("\nNOMBRE DEL JUGADOR:");
-                    nombreJugador = sc.nextLine();
-                    //imprime las opciones del tablero en la pantalla
-                    System.out.println("\n/////////////////////////////////////////"); 
-                    System.out.println("ESCOJE EL TAMANO DEL TABLERO CON LA LETRA");
-                    System.out.println("/////////////////////////////////////////");
-                    System.out.println("P. TABLERO PEQUENO 5X6");
-                    System.out.println("G. TABLERO GRANDE 10X10");
-                    tamaño = sc.next().charAt(0); 
+            case 1:
+                sc.nextLine(); 
+                System.out.println("\nNOMBRE DEL JUGADOR:");
+                nombreJugador = sc.nextLine();
+                System.out.println("\n/////////////////////////////////////////"); 
+                System.out.println("ESCOJE EL TAMANO DEL TABLERO CON LA LETRA");
+                System.out.println("/////////////////////////////////////////");
+                System.out.println("P. TABLERO PEQUENO 5X6");
+                System.out.println("G. TABLERO GRANDE 10X10");
+                tamaño = sc.next().toUpperCase().charAt(0); 
 
-                    //Se usa While para que siga solo si es P o G
-                    while(tamaño != 'P' && tamaño != 'G'){
-                        System.out.println("COLOCAR UNA LETRA VALIDA");
-                        tamaño = sc.next().charAt(0);
-                    }
+                while(tamaño != 'P' && tamaño != 'G'){
+                    System.out.println("COLOCAR UNA LETRA VALIDA");
+                    tamaño = sc.next().toUpperCase().charAt(0);
+                }
 
-                    //se craaron variables ya que dependen de lo que escoja el usuario
-                    if (tamaño == 'P'){
-                      filasActuales = FILAS_PEQUENO;
-                      columnasActuales = COLUMNAS_PEQUENO;
-            
-                    }else{
-                       filasActuales = FILAS_GRANDE;
-                      columnasActuales = COLUMNAS_GRANDE;
+                if (tamaño == 'P'){
+                    filasActuales = FILAS_PEQUENO;
+                    columnasActuales = COLUMNAS_PEQUENO;
+                }else{
+                    filasActuales = FILAS_GRANDE;
+                    columnasActuales = COLUMNAS_GRANDE;
+                }
+              
+                numeroPremios = asignarCantidades("PREMIOS [0] (MIN:1)", 0.4);
+                int numEspeciales = asignarCantidades("PREMIOS [$] (MIN:1)", 0.1); // Agregado para el $
+                numeroParedes = asignarCantidades("PAREDES [X] (MIN:1)", 0.2);
+                numeroTrampas = asignarCantidades("FANTASMAS [@](MIN:1)", 0.2);
+             
+                System.out.println("\nGENERANDO TABLERO...");
+     
+                tablero = new String [filasActuales][columnasActuales];
+                for (int i = 0; i < filasActuales; i++){
+                    for (int j = 0; j < columnasActuales; j++){
+                        tablero[i][j] = " ";
                     }
-                  
-                    numeroPremios = asignarCantidades("PREMIOS [0] (MIN:1)", 0.4);
-                    numeroParedes = asignarCantidades("PAREDES [X] (MIN:1)", 0.2);
-                    numeroTrampas = asignarCantidades("FANTASMAS [@](MIN:1)", 0.2);
+                }
+
+                colocarElemento(tablero, premio, numeroPremios);
+                colocarElemento(tablero, premio_especial, numEspeciales);
+                colocarElemento(tablero, pared, numeroParedes);
+                colocarElemento(tablero, fantasma, numeroTrampas);
+                
+                System.out.println("TABLERO DE "+filasActuales+"X"+columnasActuales+" CREADO!!!, A JUGAR");
+                imprimirTablero(tablero);
                  
-                    System.out.println("\nGENERANDO TABLERO...");
-         
-                    //se imprime el tablero en la pantalla
-                    tablero = new String [filasActuales][columnasActuales];
-                    
-                    for (int i = 0; i < filasActuales; i++){
-                        for (int j = 0; j < columnasActuales; j++){
-                            tablero[i][j] = " ";
-                        }
+                //GENERACION DEL PERSONAJE
+                int personajeFila;
+                int personajeColumna;
+                do{
+                    System.out.println("POSICION PARA APARECER:");
+                    System.out.print("FILAS (1 a " + filasActuales + "): ");
+                    personajeFila = sc.nextInt();
+                    System.out.print("COLUMNAS (1 a " + columnasActuales + "): ");
+                    personajeColumna = sc.nextInt();
+                    if(verificacionPosicionPersonaje(personajeFila, personajeColumna, tablero) == false){
+                        System.out.println("ERROR POSICION INVALIDA, ESCOJE OTRA UBICACION!!");
                     }
+                }while(verificacionPosicionPersonaje(personajeFila, personajeColumna, tablero) == false);
+           
+                System.out.println("GENERACION EXITOSA!");
+                pacmanFila = personajeFila - 1;
+                pacmanCol = personajeColumna - 1;
+                tablero[pacmanFila][pacmanCol] = pacman;
 
-                    //Poniendo premios en la matriz
-                    colocarElemento(tablero, premio, numeroPremios);
-                    
-                    //Poniendo las paredes en la matriz
-                    colocarElemento(tablero, pared, numeroParedes);
-                    
-                    //Poniendo fantasmas
-                    colocarElemento(tablero, fantasma, numeroTrampas);
-                    
-                    System.out.println("TABLERO DE "+filasActuales+"X"+columnasActuales+" CREADO!!!, A JUGAR");
-                    
+                //TODO LO QUE ESTA EN ESTE BUCLE CONFIGURA EL JUEGO
+                jugando = true;
+                while (jugando) {
+                    System.out.println("\nJUGADOR: " + nombreJugador + " | PUNTOS: " + puntos + " | VIDAS: " + vidas);
                     imprimirTablero(tablero);
-                    System.out.println();
+                    System.out.print("Mover (8:Arriba, 5:Abajo, 4:Izq, 6:Der, F:Pausa): ");
+                    char tecla = sc.next().charAt(0);
                     
-                    int personajeFila;
-                    int personajeColumna;
-                    //este ciclo dice el rango para que el personaje aparesca
-                    do{
-                        System.out.println("POSICION PARA APARECER:");
-                        System.out.print("FILAS (1 a " + filasActuales + "): ");
-                        personajeFila = sc.nextInt();
-                        System.out.print("COLUMNAS (1 a " + columnasActuales + "): ");
-                        personajeColumna = sc.nextInt();
-                        if(verificacionPosicionPersonaje(personajeFila, personajeColumna, tablero) == false){
-                            System.out.println("ERROR POSICION INVALIDA, ESCOJE OTRA UBICACION!!");
+                    //ESTE CICLO NOS INDICA LA CONFIGURACION DE LA PAUSA Y SU TECLA
+                    if (tecla == 'F') {
+                        System.out.println("1. Continuar | 2. Salir al menu");
+                        int pausa = sc.nextInt();
+                        
+                        if (pausa == 2) {
+                            jugando = false;
                         }
-                    }while(verificacionPosicionPersonaje(personajeFila, personajeColumna, tablero) == false);
-                    
-                    
-                    System.out.println("GENERACION EXITOSA!");
-                    tablero[personajeFila-1][personajeColumna-1] = pacman;
-                    imprimirTablero(tablero);
-                    
-                    if (controles == f){
-                        System.out.println("JUEGO EN PAUSA:");
-                        System.out.println("1.CONTINUAR JUGANDO");
-                        System.out.println("2.SALIR DEL JUEGO");
+                    } else {
+                        movimientoPacman(tecla); // MOVIEMIENTOPACMAN ES UNA CLASE 
                     }
-
-                    break;
-                    // caso 2 se usa para el historial de puntos
-                case 2:
-                    System.out.println("--------------------------------------");
-                    System.out.println("NOMBRE DEL JUGADOR: " +nombreJugador);
-                    //System.out.println("PUNTOS TOTALES: ", puntos);
-                    
-                    System.out.println("--------------------------------------");
-                    break;
-                    // es la salida del juego
-                case 3:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Opción inválida. Intenta de nuevo");
+                    //ESTE CICLO NOS INDICA CUANDO EL USUARIO HA PERDIDO
+                    if (vidas <= 0) {
+                        System.out.println("--- JUEGO TERMINADO :( ---");
+                        jugando = false;
+                    }
+                }
+                break;
+                // SE CONFIGURA EL HISTORIAL DE PUNTOS
+            case 2:
+                System.out.println("--------------------------------------");
+                System.out.println("NOMBRE DEL JUGADOR: " + nombreJugador);
+                System.out.println("PUNTOS TOTALES: " + puntos);
+                System.out.println("--------------------------------------");
+                break;
+                //ESTA CLASE NOS SACA DEL JUEGO
+            case 3:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Opción inválida. Intenta de nuevo");
         }
     }
-    
-    public static void imprimirTablero(String[][] tablero) {
-        // Borde superior
-        for (int j = 0; j < columnasActuales + 2; j++) {
-            System.out.print("--");
-        }
-        System.out.println();
 
-        // Filas del tablero
+    //MOVIEMINTO DEL PACMAN
+    public static void movimientoPacman(char tecla) {
+        int moverFila = pacmanFila;
+        int moverColumna = pacmanCol;
+        //CONFIGURA LAS TECLAS DE MOVIEMIENTO DEL PACMAN
+        if (tecla == '8') moverFila--;
+        else if (tecla == '5') moverFila++;
+        else if (tecla == '4') moverColumna--;
+        else if (tecla == '6') moverColumna++;
+        else return;
+
+        //BORDEN INFINITOS (SE VA DE UN LADO Y REGRESA A OTRO)
+        if (moverFila < 0) moverFila = filasActuales - 1;
+        if (moverFila >= filasActuales) moverFila = 0;
+        if (moverColumna < 0) moverColumna = columnasActuales - 1;
+        if (moverColumna >= columnasActuales) moverColumna = 0;
+
+        //CHOQUE CON PAREDES OSEA LAS X
+        if (tablero[moverFila][moverColumna].equals(pared)) {
+            System.out.println("¡PARED!");
+            return;
+        } 
+        //DITRIBUCION DE PREMIOS (0 , $)Y FANTASMAS @
+        if (tablero[moverFila][moverColumna].equals(premio)){ puntos += 10;
+            System.out.println("PREMIO SIMPLE +10 PUNTOS");
+        }else if (tablero[moverFila][moverColumna].equals(premio_especial)){ puntos += 25;
+              System.out.println("!!!PREMIO ESPECIAL !!! +25 PUNTOS");
+        }else if (tablero[moverFila][moverColumna].equals(fantasma)) {
+            vidas--;
+            System.out.println("PERDISTE UNA VIDA! :c");
+        }
+
+        // MOVER EN EL ARREGLO
+        tablero[pacmanFila][pacmanCol] = " ";
+        pacmanFila = moverFila;
+        pacmanCol = moverColumna;
+        tablero[pacmanFila][pacmanCol] = pacman;
+    }
+    //IMPRESION DEL TABLERO 
+    public static void imprimirTablero(String[][] tablero) {
+        for (int j = 0; j < columnasActuales + 2; j++) System.out.print("--");
+        System.out.println();
         for (int i = 0; i < filasActuales; i++) {
             System.out.print("|");
             for (int j = 0; j < columnasActuales; j++) {
@@ -168,16 +213,11 @@ public class PacmanS1 {
             }
             System.out.println("|");
         }
-
-        // Borde inferior
-        for (int j = 0; j < columnasActuales + 2; j++) {
-            System.out.print("--");
-        }
+        for (int j = 0; j < columnasActuales + 2; j++) System.out.print("--");
         System.out.println();
     }
-    
+    // VERIFICA SI LA POSICION DEL PACMAN ES CORRECTA 
     public static boolean verificacionPosicionPersonaje(int x, int y, String[][] tablero){
-        // Verificamos que no se salga de la matriz antes de revisar el contenido
         if(x >= 1 && x <= filasActuales && y >= 1 && y <= columnasActuales){
             if(tablero[x-1][y-1].equals(" ")){
                 return true;
@@ -185,37 +225,32 @@ public class PacmanS1 {
         }
         return false;
     }
-    
+    //ESTE METODO ADIGANA LA CANTIDAD DE LOS OBJETOS DEPENDE DEL TABLERO
     public static int asignarCantidades(String objeto, double porcentaje){
         boolean bandera = false;
         int cantidad = 0;
-        int maxPremios = (int)(filasActuales * columnasActuales * porcentaje);
-        //sirve para imprimir la cantidad de objetos en el tablero
+        int maxItems = (int)(filasActuales * columnasActuales * porcentaje);
         while(bandera == false){
-            System.out.print("ELIJE LA CANTIDAD DE "+objeto + " (MAX:"+ maxPremios+")" ) ;
+            System.out.print("CANTIDAD DE "+objeto + " (MAX:"+ maxItems+"): " ) ;
             cantidad = sc.nextInt();
-              
-            if (cantidad >= 1 && cantidad <= maxPremios) {
+            if (cantidad >= 1 && cantidad <= maxItems) {
                 bandera = true;
             } else {
-                System.out.println("ERROR!!!. INGRESE UNA CANTIDAD QUE ESTE EN EL RANGO.");
+                System.out.println("ERROR!!!. FUERA DE RANGO.");
             }  
         }
         return cantidad;
     }
-    //coloca los elementos en el tablero
+    //ESTA CLASE NOS INDICA COMO SE COLOCAN LOS OBJETOS DE FORMA ALEATORIA
     public static void colocarElemento(String[][] tablero, String simbolo, int cantidad) {
         int colocados = 0;
-
         while (colocados < cantidad) {
             int fila = rand.nextInt(filasActuales);
             int columna = rand.nextInt(columnasActuales);
-
             if (tablero[fila][columna].equals(" ")) {
                 tablero[fila][columna] = simbolo;
                 colocados++;
             }
         }
     }
-
 }
